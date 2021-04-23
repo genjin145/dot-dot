cc8eb621c4953391b82a252e772c8b5b0f79eb18
GenJin145 <genjin145@gmail.com>
2021-04-23 16:10:53 +0500

    change build

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  target: 'web',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'js/index.js',
   },
   mode: 'development',
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   devServer: {
     contentBase: path.join(__dirname, 'build'),
     compress: true,
     port: 4000,
+    open: true,
   },
   module: {
     rules: [
@@ -22,15 +24,34 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader",
+          'css-loader',
+          'sass-loader',
         ],
       },
-    ],
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name][ext]'
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          publicPath: '../',
+          filename: 'fonts/[name][ext]'
+        }
+      }
+    ]
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({template: 'src/index.html'}),
-    new MiniCssExtractPlugin({filename: 'css/style.css'})
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/style.css'
+    })
   ],
 };
